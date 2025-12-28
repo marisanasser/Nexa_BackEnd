4f85efa4867e56b845886feb0e2c7821a6a74276
= <=>
2025-12-28 00:08:53 -0300

    feat(seeder): add admin user seeder

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminUserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $admin = User::updateOrCreate(
+            ['email' => 'admin@nexacreators.com.br'],
+            [
+                'name' => 'Admin Nexa',
+                'password' => Hash::make('NexaAdmin@2025'),
+                'role' => 'admin',
+                'email_verified_at' => now(),
+                'has_premium' => true,
+            ]
+        );
+
+        $this->command->info("Admin user created/updated: {$admin->email}");
+    }
+}
