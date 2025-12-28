41afe05d0eaa98b154a6e8339deb11a831860c44
= <=>
2025-12-27 23:14:49 -0300

    fix(cors): add new frontend URL to allowed origins

--- config/cors.php
+++ config/cors.php
@@ -18,6 +18,7 @@ return [
         'http://127.0.0.1:5001',
         'http://127.0.0.1:5173',
         'https://nexa-frontend-1044548850970.southamerica-east1.run.app',
+        'https://nexa-frontend-bwld7w5onq-rj.a.run.app',
     ])),
 
     'allowed_origins_patterns' => [],
