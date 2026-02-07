47f00d3e3116ad9f53c2a1031fbadc970323e410
= <=>
2026-02-07 13:32:24 -0300

    fix: use semicolon for SANCTUM_STATEFUL_DOMAINS to avoid gcloud comma parsing issues

--- config/sanctum.php
+++ config/sanctum.php
@@ -5,12 +5,12 @@ declare(strict_types=1);
 use App\Http\Middleware\EncryptCookies;
 
 return [
-    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
+    'stateful' => explode(',', str_replace(';', ',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s%s',
         'localhost,localhost:5000,127.0.0.1,127.0.0.1:8000,::1',
         env('APP_URL') ? ','.parse_url(env('APP_URL'), PHP_URL_HOST) : '',
         env('FRONTEND_URL') ? ','.parse_url(env('FRONTEND_URL'), PHP_URL_HOST) : ''
-    ))),
+    )))),
 
     'guard' => ['web'],
 
